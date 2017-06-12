71116887f50d0fdeedd58f1c9624c4ffa88f4835
jannispriesnitz <jannis-priesnitz@freenet.de>
2017-06-12 20:56:32 +0200

    New style no clean up.

---
+++ Term_Paper/make-proceedings.bat
@@ -0,0 +1,14 @@
+@echo off
+pdflatex -synctex=1 proceedings
+pdflatex -synctex=1 proceedings
+rem TOC should be correct now
+sed -i "s/\\\\IeC //g" proceedings.bib
+biber proceedings
+pdflatex -synctex=1 proceedings
+texindy proceedings.idx
+pdflatex -synctex=1 proceedings
+pdflatex -synctex=1 proceedings
+sed -i "s/\\\\IeC //g" proceedings.bib
+sed -i "s/\\\\textunderscore //g" proceedings.bib
+sed -i "s/\\\\IeC //g" proceedings.csv
+sed -i "s/ ;/;/g" proceedings.csv
